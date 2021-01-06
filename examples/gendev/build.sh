00529f17cbc5abb38b3eb5ed09c6ca5ad94ade76
Dan Leslie <dan@ironoxide.ca>
2021-01-06 12:05:44 -0800

    no aliases

--- examples/gendev/build.sh
+++ examples/gendev/build.sh
@@ -4,7 +4,7 @@ for x in *
 do
     echo -e "Building $x\n\n"
     pushd $x
-    make-gendev
+    docker container run --rm --name gendev -v `pwd`:/src --user $(id -u):$(id -g) dleslie/gendev
     popd
     echo -e "\n\n5 second pause\n\n"
     sleep 5
--- examples/marsdev/build.sh
+++ examples/marsdev/build.sh
@@ -4,7 +4,7 @@ for x in *
 do
     echo -e "Building $x\n\n"
     pushd $x
-    make-marsdev
+    docker container run --rm --name marsdev -v `pwd`:/data --user $(id -u):$(id -g) dleslie/marsdev make -C data
     popd
     echo -e "\n\n5 second pause\n\n"
     sleep 5
