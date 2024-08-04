0e3ea853db2ec070009b246864ba601485b55ec2
Dan Leslie <dan@ironoxide.ca>
2024-08-04 11:42:52 -0700

    ...

--- examples/marsdev/sgdk-skeleton/out/res/resources.d
+++
@@ -1,2 +0,0 @@
-out/res/resources.o: res/resources.res \
-/data/sgdk-skeleton/res/moon.bmp
--- examples/marsdev/sgdk-stage9-sample/out/res/resources.d
+++
@@ -1,4 +0,0 @@
-out/res/resources.o: res/resources.res \
-/data/sgdk-stage9-sample/res/tiny16.png \
-/data/sgdk-stage9-sample/res/character.png \
-/data/sgdk-stage9-sample/res/world.s9m
